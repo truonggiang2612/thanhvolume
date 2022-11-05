a181c3b08ceb6246bf1986883aa12117dfdf3f9b
truonggiang2612 <toidaumuonkhocprolaanhne@gmail.com>
2022-11-05 16:00:27 +0700

    thanh volume

---
+++ main.js
@@ -0,0 +1,9 @@
+const valueNumber = document.getElementById('rangeValue');
+
+function rangeSlider(value) {
+    valueNumber.innerHTML = value;
+}
+
+// Tiếp theo mình sẽ hướng dẫn các bạn push lên github nhé
+// các bạn vào github.com và tạo 1 respon trước nhé, nó là
+// cái kho chứa project này đấy
